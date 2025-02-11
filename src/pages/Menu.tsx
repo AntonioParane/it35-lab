9a65ee2d507c53c3d3403f16b6c9c1fd70411c0d
AntonioParane <anthon2712@gmail.com>
2025-02-11 19:29:20 +0800

    added logout in  menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -47,7 +47,10 @@ import Details from './Details';
                             </IonMenuToggle>
                         ))}
 
-                        
+                        {/*Logout Button*/}
+                        <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
+                            <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
+                        Logout
                         </IonButton>
                         
                     </IonContent>
