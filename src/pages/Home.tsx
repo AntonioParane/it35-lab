a8066f5f547b1ba22dc55efef1fbcc4a63894ecb
AntonioParane <anthon2712@gmail.com>
2025-02-18 08:52:12 +0800

    Added Tabs setup and home-tabs/pages/updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,27 +5,33 @@ import {
       IonHeader, 
       IonMenuButton, 
       IonPage, 
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabs, 
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router';
+import Feed from './home-tabs/Feed';
   
   const Home: React.FC = () => {
+
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton ></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonButton routerLink="/it35-lab/app/home/details" expand="full">
-              Details
-          </IonButton>
-        </IonContent>
-      </IonPage>
+      <IonReactRouter>
+        <IonTabs>
+          <IonTabBar slot="bottom">
+
+          </IonTabBar>
+          <IonRouterOutlet>
+            <Route exact path="/it35-lab/app/home/feed" component={Feed} />
+            <Route exact path="/it35-lab/app/home/search" component={Search} />
+            <Route exact path="/it35-lab/app/home/favorites" component={Favorites} />
+          
+            <Route exact path="/it35-lab/app/home"> 
+          </IonRouterOutlet>
+        </IonTabs>
+      </IonReactRouter>
     );
   };
   
