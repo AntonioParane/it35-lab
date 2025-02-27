e6e202d1da19cad469524e87f1b42a45324338ab
AntonioParane <anthon2712@gmail.com>
2025-02-27 21:49:55 +0800

    added output for search.tsx

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,39 +1,51 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Search: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Search</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Search
-          </div>
-  
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Search;
+import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonLabel
+} from '@ionic/react';
+
+const Search: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Search</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        {/* Search Bar */}
+        <IonSearchbar placeholder='Search for a house'></IonSearchbar>
+        
+        {/* Recent Searches */}
+        <IonList>
+          <IonItem>
+            <IonLabel>🏡 Manila - ₱500,000</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>🏡 Quezon City - ₱750,000</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>🏡 Makati - ₱450,000</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>🏡 Tagaytay - ₱600,000</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Search;
