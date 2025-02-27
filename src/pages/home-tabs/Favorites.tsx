302316eb8930cb907f8ab2d9ee84810277716de8
AntonioParane <anthon2712@gmail.com>
2025-02-27 22:06:08 +0800

    added output for favorites.tsx

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -1,38 +1,54 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Favorites: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Favorites</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Favorites
-          </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
+import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel
+} from '@ionic/react';
 
-  export default Favorites;
+const Favorites: React.FC = () => {
+  const favoriteHouses = [
+    { name: 'Manila', price: '₱500,000' },
+    { name: 'Quezon City', price: '₱750,000' },
+    { name: 'Makati', price: '₱450,000' },
+    { name: 'Tagaytay', price: '₱600,000' },
+    { name: 'Pasig', price: '₱550,000' },
+    { name: 'Caloocan', price: '₱620,000' },
+    { name: 'Mandaluyong', price: '₱700,000' },
+    { name: 'Parañaque', price: '₱580,000' },
+    { name: 'Las Piñas', price: '₱490,000' },
+    { name: 'Marikina', price: '₱510,000' }
+  ];
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Favorites</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          {favoriteHouses.map((house, index) => (
+            <IonItem key={index}>
+              <IonLabel>
+                🏡 {house.name} - {house.price}
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Favorites;
