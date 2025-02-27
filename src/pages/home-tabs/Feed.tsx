d2c91418d0e8936248bbedcfc9f598c8ff4d304d
AntonioParane <anthon2712@gmail.com>
2025-02-27 22:02:46 +0800

    added output for feed.tsx

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,58 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Feed
-          </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Feed;
+import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption
+} from '@ionic/react';
+
+const Feed: React.FC = () => {
+  const places = [
+    { name: 'Manila', price: '₱500,000' },
+    { name: 'Quezon City', price: '₱750,000' },
+    { name: 'Makati', price: '₱450,000' },
+    { name: 'Tagaytay', price: '₱600,000' }
+  ];
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          {places.map((place, index) => (
+            <IonItemSliding key={index}>
+              <IonItem>
+                <IonLabel>
+                  🏡 {place.name} - {place.price}
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption color="primary">Add to Favorites</IonItemOption>
+                <IonItemOption color="secondary">Call</IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
