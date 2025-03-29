910265423dccbd711fa3d4afec516a325e031757
AntonioParane <anthon2712@gmail.com>
2025-03-29 15:33:30 +0800

    Updates

--- assets/swipe-back-legacy-Bfynj12v.js
+++ assets/swipe-back-legacy-Bfynj12v.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cp2KS4vr.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DCPJMax6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
