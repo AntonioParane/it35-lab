3f4efb8a797e9a591343e44a3fae4cd1a409d960
AntonioParane <anthon2712@gmail.com>
2025-04-29 18:55:45 +0800

    added icon for favorites

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ const Home: React.FC = () => {
       name: 'Favorites',
       tab: 'favorites',
       url: '/it35-lab/app/home/favorites',
-      icon: 'https://media.giphy.com/media/3oz8xSsAsfYEoVljD6/giphy.gif'
+      icon: 'https://cdn.pixabay.com/animation/2023/08/21/13/31/13-31-04-403_512.gif'
     }
   ];
 
