c2ded836a40ac9f8f8c653842727bd0a80d0d37a
AntonioParane <anthon2712@gmail.com>
2025-04-29 18:53:06 +0800

    added icon for feedback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,74 @@
-import { 
+import {
   IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonIcon, 
-      IonLabel, 
-      IonMenuButton, 
-      IonPage, 
-      IonRouterOutlet, 
-      IonTabBar, 
-      IonTabButton, 
-      IonTabs, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonLabel,
+  IonMenuButton,
+  IonPage,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { bookOutline, search, star } from 'ionicons/icons';
 import { Route, Redirect } from 'react-router';
 
 import Favorites from './home-tabs/Favorites';
 import Feed from './home-tabs/Feed';
 import Search from './home-tabs/Search';
-  
-  const Home: React.FC = () => {
 
-    const tabs = [
-      {name:'Feed', tab:'feed',url: '/it35-lab/app/home/feed', icon: bookOutline},
-      {name:'Search', tab:'search', url: '/it35-lab/app/home/search', icon: search},
-      {name:'Favorites',tab:'favorites', url: '/it35-lab/app/home/favorites', icon: star},
-    ]
-    
-    return (
-      <IonReactRouter>
-        <IonTabs>
-          <IonTabBar slot="bottom">
+const Home: React.FC = () => {
+  const tabs = [
+    {
+      name: 'Feed',
+      tab: 'feed',
+      url: '/it35-lab/app/home/feed',
+      icon: 'https://cdn-icons-gif.flaticon.com/9583/9583344.gif'
+    },
+    {
+      name: 'Search',
+      tab: 'search',
+      url: '/it35-lab/app/home/search',
+      icon: 'https://media.giphy.com/media/X7yG1lAl5C5VK/giphy.gif'
+    },
+    {
+      name: 'Favorites',
+      tab: 'favorites',
+      url: '/it35-lab/app/home/favorites',
+      icon: 'https://media.giphy.com/media/3oz8xSsAsfYEoVljD6/giphy.gif'
+    }
+  ];
 
-            {tabs.map((item, index) => (
-              <IonTabButton key={index} tab={item.tab} href={item.url}>
-                <IonIcon icon={item.icon} />
-                <IonLabel>{item.name}</IonLabel>
-              </IonTabButton>
-            ))}
-            
-          </IonTabBar>
+  return (
+    <IonReactRouter>
+      <IonTabs>
         <IonRouterOutlet>
-
           <Route exact path="/it35-lab/app/home/feed" render={Feed} />
           <Route exact path="/it35-lab/app/home/search" render={Search} />
           <Route exact path="/it35-lab/app/home/favorites" render={Favorites} />
-
           <Route exact path="/it35-lab/app/home">
             <Redirect to="/it35-lab/app/home/feed" />
           </Route>
-
         </IonRouterOutlet>
-        </IonTabs>
-      </IonReactRouter>
-    );
-  };
-  
-  export default Home;
+
+        <IonTabBar slot="bottom">
+          {tabs.map((item, index) => (
+            <IonTabButton key={index} tab={item.tab} href={item.url}>
+              <img
+                src={item.icon}
+                alt={`${item.name} icon`}
+                style={{ width: 24, height: 24 }}
+              />
+              <IonLabel>{item.name}</IonLabel>
+            </IonTabButton>
+          ))}
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  );
+};
+
+export default Home;
